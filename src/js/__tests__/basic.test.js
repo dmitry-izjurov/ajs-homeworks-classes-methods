3ea03829baa09dcf09e0b0a4dea1bd7d9ddd30b3
Dmitry <dmitry.izjurov@yandex.ru>
2022-04-10 19:55:54 +0300

    Добавлен тест функции damage

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -53,18 +53,12 @@ test('Объект должен создаваться', () => {
   });
 });
 
-test('Объект не должен создаваться', () => {
-  const unit = new Zombie('Zombie', 'Zombie');
-  unit.damage();
-  expect(() => unit.levelUp()).toThrow();
+test('Функция должна считать результат', () => {
+  const unit = new Zombie('zombie', 'zombie').damage(30);
+  expect(unit).toBe(73);
 });
 
 test('Функция должна считать результат', () => {
-  const unit = new Character('zombie', 'zombie').damage(30);
-  expect(unit).toBe(82);
-});
-
-test('Функция должна считать результат', () => {
-  const unit = new Character('zombie', 'zombie').damage(300);
+  const unit = new Zombie('zombie', 'zombie').damage(300);
   expect(unit).toBe(0);
 });
